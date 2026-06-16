bf07604c1b02fda51599cce76300e8e4d17fa876
vitor1aSj <vitoria.vih.silvah@gmail.com>
2026-06-16 20:59:52 -0300

    Adiciondo medias

---
+++ media_aluno.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digite a primeira nota:"))  #8.5
+nota2 = float(input("Digite a segunda nota :"))
+nota3 = float(input("Digite a terceira nota:"))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7 :
+    print(f"Aluno(a) Aprovado!Com a media: {media:.2f}")
+else: print(f"Aluno(a) Reprovado!Com a media:{media:.2f}")
+
