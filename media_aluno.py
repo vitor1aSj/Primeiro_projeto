a7c76838429a45681e840dba24bcdbd43b96d09b
Vitória Silva <vitoria.vih.silvah@gmail.com>
2026-06-16 21:05:21 -0300

    Fix formatting and improve readability in media_aluno.py

--- media_aluno.py
+++ media_aluno.py
@@ -6,5 +6,7 @@ media = (nota1 + nota2 + nota3) / 3
 
 if media >= 7 :
     print(f"Aluno(a) Aprovado!Com a media: {media:.2f}")
+elif media >= 3 and media < 7:
+    print(f"Aluno em recuperação com media {media:.2f}")
 else: print(f"Aluno(a) Reprovado!Com a media:{media:.2f}")
 
